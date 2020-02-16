861be05260bd9d6920c16d7f1a84352468a0d5c3
nabilagita <47941007+nabilagita@users.noreply.github.com>
2020-02-16 22:57:10 +0700

    tugas 2

---
+++ Tugas1/app/src/main/java/com/example/tugas1/third.java
@@ -0,0 +1,47 @@
+package com.example.tugas1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class third extends AppCompatActivity {
+    private Button btnHasil;
+    private TextView tvHasil;
+    private EditText etAlas, etTinggi;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+
+        btnHasil = findViewById(R.id.btn_hasil);
+        tvHasil = findViewById(R.id.tv_hasil);
+        etAlas = findViewById(R.id.et_alas);
+        etTinggi = findViewById(R.id.et_tinggi);
+
+        btnHasil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    String sAlas = etAlas.getText().toString();
+                    String sTinggi = etTinggi.getText().toString();
+
+                    double alas = Double.parseDouble(sAlas);
+                    double tinggi = Double.parseDouble(sTinggi);
+
+                    double hasil = alas * tinggi;
+
+                    String sHasil = String.valueOf(hasil);
+                    tvHasil.setText(sHasil);
+                }
+                catch (NumberFormatException nfe){
+                    Toast.makeText(getApplicationContext(), "Field tidak boleh kosong!!",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
